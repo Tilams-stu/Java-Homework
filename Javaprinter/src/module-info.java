da51e9fd452bae5cd5ba96a05975f3237ad633f2
Tilams-stu <1596924998@qq.com>
2024-11-21 12:33:08 +0800

    Add files via upload

---
+++ Javaprinter/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Javaprinter {
+	requires java.desktop;
+}
---
+++ Javaprinter/src/var/Vars.java
@@ -0,0 +1,15 @@
+package var;
+import control.Controller;
+import model.LeftBar;
+import model.NorthPanel;
+import model.SouthPanel;
+import view.MyPanel;
+
+public class Vars {
+
+	public static NorthPanel northPanel = new NorthPanel();
+	public static MyPanel paintPanel = new MyPanel();
+	public static LeftBar leftBar = new LeftBar();
+	public static Controller controller = new Controller();
+	public static SouthPanel southPanel = new SouthPanel();
+}
